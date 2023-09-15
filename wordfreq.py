bae856c28fd1323c5abaf5f8619da862173a0829
Oscar Norman <oscarnormn@gmail.com>
2023-09-15 21:31:05 +0200

    Counting words, sorting them and printing them

--- wordfreq.py
+++ wordfreq.py
@@ -25,3 +25,26 @@ def tokenize (lines):
                 words.append(line[start])
             start = start+1
     return words
+
+def countWords (words, stopWords):
+    dictionary = {}
+    for word in words:
+        if word in stopWords:
+            continue
+        if word in dictionary:
+            dictionary[word] = dictionary[word]+1
+        else:
+            dictionary[word] = 1
+    return dictionary
+
+def printTopMost (frequencies, n):
+    sorted_frequencies = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
+
+    if len(sorted_frequencies) < n:
+        n = len(sorted_frequencies)
+
+    for i in range(n):
+        word = sorted_frequencies[i][0]
+        count = sorted_frequencies[i][1]
+        output = str(word).ljust(20) + str(count).rjust(5)
+        print(output)
