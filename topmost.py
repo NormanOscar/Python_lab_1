f24682c706b4e4268a9cc7adf49ff8ef2274a07c
Oscar Norman <oscarnormn@gmail.com>
2023-09-16 23:16:16 +0200

    added comments to functions and other parts

--- topmost.py
+++ topmost.py
@@ -2,11 +2,14 @@ import wordfreq
 import sys
 import urllib.request
 
+# Starts program from the command line arguments
 def main():
     is_file = False
+    # If the second argument is a URL, open the URL and read the contents
     if sys.argv[2].startswith("http://") or sys.argv[2].startswith("https://"):
         response = urllib.request.urlopen(sys.argv[2])
         inp_file = response.read().decode("utf8").splitlines()
+    # Otherwise, open the file
     else:
         is_file = True
         inp_file = open(sys.argv[2])
@@ -16,6 +19,7 @@ def main():
 
     stop_words = []
 
+    # Read the stop words into a list
     for line in stop_word_file:
         stripped_line = line.strip()
         stop_words.append(stripped_line)
@@ -24,9 +28,11 @@ def main():
     frequencies = wordfreq.countWords(words, stop_words)
     wordfreq.printTopMost(frequencies, amount)
 
+    # Close the file if it was opened
     if is_file:
         inp_file.close()
     stop_word_file.close()
 
+# Starts the program
 if __name__ == "__main__":
     main()
--- wordfreq.py
+++ wordfreq.py
@@ -1,11 +1,14 @@
+# Splits lines of text into list of words
 def tokenize (lines):
     words = []
     for line in lines:
         start = 0
         while start < len(line):
+            # Skip whitespace
             if line[start].isspace():
                 start = start+1
                 continue
+            # If the character is a letter, add it to the word
             if line[start].isalpha():
                 end = start
                 while end < len(line) and line[end].isalpha():
@@ -13,6 +16,7 @@ def tokenize (lines):
                 words.append((line[start:end]).lower())
                 start = end
                 continue
+            # If the character is a digit, add it to the word
             elif line[start].isdigit():
                 end = start
                 while end < len(line) and line[end].isdigit():
@@ -20,28 +24,36 @@ def tokenize (lines):
                 words.append((line[start:end]).lower())
                 start = end
                 continue
+            # If the character is a symbol, add it to the word
             else:
                 words.append(line[start])
             start = start+1
     return words
 
+# Counts the number of times each word appears in the list of words
 def countWords (words, stopWords):
     dictionary = {}
     for word in words:
+        # Skip stop words
         if word in stopWords:
             continue
+        # If the word is already in the dictionary, increment its count
         if word in dictionary:
             dictionary[word] = dictionary[word]+1
+        # Otherwise, add it to the dictionary
         else:
             dictionary[word] = 1
     return dictionary
 
+# Prints the top n most frequent words
 def printTopMost (frequencies, n):
     sorted_frequencies = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
 
+    # If the number of words is less than n, set n to the number of words
     if len(sorted_frequencies) < n:
         n = len(sorted_frequencies)
-
+    
+    # Print the top n most frequent words
     for i in range(n):
         word = sorted_frequencies[i][0]
         count = sorted_frequencies[i][1]
